2c1836c0166ad7ae63cf92f5b1134c20ba900ff4
AKSHAY KUMAR MISHRA <akshayk.kvhec@gmail.com>
2020-06-08 00:39:26 +0530

    MaxOccuringChar Updated

--- GeeksForGeeks DSA Foundation/src/strings/MaximumOccuringChar.java
+++ GeeksForGeeks DSA Foundation/src/strings/MaximumOccuringChar.java
@@ -1,73 +1,70 @@
+/*
+ *   Given a string str. The task is to find the maximum 
+ *   occurring character in the string str. If more than 
+ *   one character occurs maximum number of time then print 
+ *   the lexicographically smaller character.
+ *   
+ *   
+ *   Solution : It can be done by storing the the count of
+ *              characters in the array and simultaneously 
+ *              keep the max count in a variable.
+ *              
+ *              Once the string is processed , traverse the 
+ *              array and return the first character whoose
+ *              count is equal to max
+ *   
+ *          imp :  'a'-'a' = 0 
+ *                 'b'-'a' = 1
+ *                 'c'-'a' = 1
+ *   
+ *   
+ * 
+ * */
+
 package strings;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class MaximumOccuringChar {
-	
+
 	static char maxOcc(String str) {
-		
-	
-	        
-		      Map<Character , Integer> mp = new LinkedHashMap<>();
-		      int  max     = Integer.MIN_VALUE;
-		      char maxChar ='a';
-		      
-		      for(int i = 0 ; i < str.length() ; i++){
-		          
-		          
-		            
-		            if(mp.containsKey(str.charAt(i))){
-		                
-		                mp.put(str.charAt(i)  , mp.get(str.charAt(i))+1);
-		                
-		            }
-		            else
-		               mp.put(str.charAt(i) , 1);
-		            
-		        
-		      }
-		      
-		      int arr[] = new int[26];
-		      
-		      for(Map.Entry entry : mp.entrySet()){
-		            
-		            if((int)entry.getValue() > max){
-		                 
-		                 max = (int)entry.getValue();
-		                 maxChar = (char)entry.getKey();
-		                 arr[(int)maxChar-'a']++;
-		            }
-		            
-		            
-		        }
-		        
-		        int ch = Character.MIN_VALUE ;
-		        for(int i = 0 ; i < arr.length ; i++){
-		            
-		            if(arr[i] == max){
-		                
-		                ch =  i+97;
-		            }
-		            
-		        }
-		      
-		      return (char)ch;        
-		    }
-		
-		
-		
-	
-	
+
+		int arr[] = new int[26];
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < str.length(); i++) {
+
+			arr[str.charAt(i) - 'a']++;
+
+			if (arr[str.charAt(i) - 'a'] > max) {
+
+				max = arr[str.charAt(i) - 'a'];
+			}
+
+		}
+
+		int res = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] == max) {
+
+				res = i + 97;
+				return (char) res;
+
+			}
+		}
+
+		return (char) res;
+
+	}
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
+
 		String str = kb.next();
-		
+
 		char res = maxOcc(str);
-		
+
 		System.out.println(res);
 
 	}
