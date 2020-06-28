ef5bd57314773763721cf54396db4b3610540ab7
AKSHAY KUMAR MISHRA <akshayk.kvhec@gmail.com>
2020-06-28 08:33:48 +0530

    Efficient Approach Added to FisrtRepeatingCharacter

--- GeeksForGeeks DSA Foundation/src/strings/FirstRepeatingCharacter.java
+++ GeeksForGeeks DSA Foundation/src/strings/FirstRepeatingCharacter.java
@@ -21,7 +21,24 @@
  *               we return it , as it is the second occurence
  *               at the minimum index;
  *    
+ *     *****UPDATE***
+ *   
+ *  1.Optimized Approach(Using Visited array)
+ *  
+ *  We can use visited arrays.
+ *  
+ *  We can start traversing from the right side of the string
+ *  and mark in the arrays of characters as visited if it is
+ *  not visited earlier.
+ *  
+ *  If a character is visited earlier then its index is
+ *  assigned to the result variable;
+ *  
+ *  
+ *  function  usingVisitedArray() implements this technique;
  * 
+ * 
+ *   
  * 
  * */
 
@@ -32,27 +49,46 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FirstRepeatingCharacter {
+	
+	static char usingVisitedArray(String str) {
+
+		boolean visited[] = new boolean[26];
+		int result = -1;
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+
+			if (visited[str.charAt(i) - 'a'] == false) {
+
+				visited[str.charAt(i) - 'a'] = true;
+			} else
+				result = i;
+
+		}
+
+		if (result == -1) {
+			return '#';
+		} else
+			return str.charAt(result);
+
+	}
 
 	static char firstRepeating(String str) {
-		
+
 		List<Character> li = new ArrayList<>();
-		
-		for(int i = 0 ; i < str.length() ; i++ ) {
-			
-			if(li.contains(str.charAt(i))) {
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (li.contains(str.charAt(i))) {
 				return str.charAt(i);
-			}
-			else{
-				
+			} else {
+
 				li.add(str.charAt(i));
 			}
-			
-			
+
 		}
-		
-		
+
 		return '1';
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -61,12 +97,18 @@ public class FirstRepeatingCharacter {
 
 		String str = kb.next();
 
-		char result = firstRepeating(str);
-		
-		if(result == '1') {
-			System.out.println("-1");
-		}
-		else
+		/*
+		 * char result = firstRepeating(str);
+		 * 
+		 * if(result == '1') { System.out.println("-1"); } else
+		 * System.out.println(result);
+		 */
+
+		char result = usingVisitedArray(str);
+
+		if (result == '#') {
+			System.out.println("Distinct Characters");
+		} else
 			System.out.println(result);
 
 	}
