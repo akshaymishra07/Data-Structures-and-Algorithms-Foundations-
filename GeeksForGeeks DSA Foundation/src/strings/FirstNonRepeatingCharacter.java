459e984fee74a34f39495abffc1411752f4ef948
AKSHAY KUMAR MISHRA <akshayk.kvhec@gmail.com>
2020-06-29 09:12:43 +0530

    Best Approach added to FirstNonRepeatingCharacter

--- GeeksForGeeks DSA Foundation/src/strings/FirstNonRepeatingCharacter.java
+++ GeeksForGeeks DSA Foundation/src/strings/FirstNonRepeatingCharacter.java
@@ -9,17 +9,73 @@
  *   to keep the count of the characters and the 
  *   time complexity is O(N) with space complexity O(N);
  * 
+ *   Note: This question is also called leftmost non-repeating
+ *         element , and it is commonly asked in technical 
+ *         interviews.
+ *         
+ *   *************************UPDATE*******************     
+ *   Use of HashMap is not always favorable as it needs 
+ *   O(n) auxiliary space. 
+ *   
+ *   The most efficient approach is using an array which 
+ *   will store the first indexes of the non-repeating
+ *   characters present in the string.
+ *   
+ *   It will take only one traversal of the string 
+ *   and O(1) auxiliary space;
+ *   
+ *   This approach is implemented in 
+ *   usingFirstIndexArray() method;
+ *    
+ *               
+ *         
  *   
  * 
  * */
 
 package strings;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class FirstNonRepeatingCharacter {
+
+	static char usingFirstIndexArray(String str) {
+		int firstIndex[] = new int[26];
+		Arrays.fill(firstIndex, -1);
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (firstIndex[str.charAt(i) - 'a'] == -1) {
+
+				firstIndex[str.charAt(i) - 'a'] = i;
+
+			}
+			else
+				firstIndex[str.charAt(i) - 'a'] = -2;
+
+		}
+		
+		int result = Integer.MAX_VALUE;
+		
+		for(int i = 0 ; i < firstIndex.length ; i++) {
+			
+			if(firstIndex[i] >= 0 ) {
+				
+				result = Math.min(result , firstIndex[i]);
+			}
+		}
+
+		if(result == Integer.MAX_VALUE ) {
+			
+			return '#';
+		}
+		else
+			return str.charAt(result);
+		
+	}
 
 	static String firstNonRep(String str) {
 
@@ -47,16 +103,22 @@ public class FirstNonRepeatingCharacter {
 		return "-1";
 	}
 
-	
-
 	public static void main(String[] args) {
 
 		Scanner kb = new Scanner(System.in);
 
 		String str = kb.next();
 
-		System.out.println(firstNonRep(str));
-        
+		//System.out.println(firstNonRep(str));
+
+		char result = usingFirstIndexArray(str);
+		
+		if(result == '#'){
+			
+			System.out.println("No Non_Repeating Character");
+		}
+		else
+			System.out.println(result);
 		
 		
 	}
