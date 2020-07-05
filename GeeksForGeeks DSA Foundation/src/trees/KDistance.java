cc07cc0c9eb4eb08c8540cb5c20a7ff5e15ec5d8
AKSHAY KUMAR MISHRA <akshayk.kvhec@gmail.com>
2020-07-05 23:29:02 +0530

    Kdistance Updated

--- GeeksForGeeks DSA Foundation/src/trees/KDistance.java
+++ GeeksForGeeks DSA Foundation/src/trees/KDistance.java
@@ -42,13 +42,28 @@
 
 
 
+/*
+package trees;
 
-/*package trees;
+import java.util.Scanner;
 
 public class KDistance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 
+		 Scanner kb = new Scanner(System.in);
+		 
+		 
+		 int n = kb.nextInt();
+		 int  arr[] = new int[n];
+		  for(int i = 0 ; i < n ; i++) {
+			  
+			  arr[i] = kb.nextInt();
+			  
+		  }
+		  
+		 
 
 	}
 
